0e7489067b55a4ef2981853cef7847aaed9b69c0
Justin Cho <justincho99@live.com>
2021-06-03 09:45:30 -0700

    Adds var_dump

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 if(isset($_GET['FirstName'])){//data exists, show it
-  echo $_GET['FirstName']; . ' ' . $_GET['LastName'];
+  //echo $_GET['FirstName']; . ' ' . $_GET['LastName'];
+
+  echo '<pre>';
+  var_dump($_GET);
+  echo '</pre>';
+
+
 }else{//no data, show form
-    echo'
+    echo '
     <form action="">
     First Name: <input type="text" name="FirstName" /><br />
     Last Name: <input type="text" name="LastName" /><br />
